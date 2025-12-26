6c0d922f1416e6acaed0ca8a170063dfbb1b0416
Sayan <sayan.banerjee262004@gmail.com>
2025-12-26 19:31:05 +0530

    Comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+// console.log(2 <= 1)
+
+
+// console.log("2" > 1);  TRUE
+// console.log("02" > 1);  TRUE
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);  //DONT CHECK EQUALITY AMONG THESE 3, INCONSISTENCY DUE TO CONVERSION TO NaN and Undefined
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined != 0);
+
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
+
