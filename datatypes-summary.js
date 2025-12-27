f4142f4523505f5c38d22fab3a050cba882d7c87
Sayan <sayan.banerjee262004@gmail.com>
2025-12-27 11:49:33 +0530

    Datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,14 @@
+// PRIMITIVE
+
+// 7 Types : Strings, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId); // false
+
+
+// Dynamically typed
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
