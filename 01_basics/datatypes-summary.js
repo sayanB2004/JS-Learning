2fa7a4a0e8db50aa016ee08b56e2e65d5d9a9e04
Sayan <sayan.banerjee262004@gmail.com>
2025-12-28 09:46:16 +0530

    Memory (Stack and Heap)

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,5 +33,34 @@ console.log(typeof data);
 console.log(typeof myFunction);
 
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "Sayan Banerjee" // Stack since primitive
+
+let anothername = myName // #### COPY PASSED
+anothername = "Sayan" 
+
+console.log(myName);
+console.log(anothername);  // BOTH NAMES ARE DIFFERENT AS ONLY COPY PASSED
+
+//Heap functionality
+
+let userOne = 
+{
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+
+let userTwo = userOne // #### REFERENCE PASSED
+
+userTwo.email = "sb@gmail"
+
+console.log(userOne.email);
+console.log(userTwo.email);  // #### BOTH EMAILS WILL BE SAME AS REFERENCE IS PASSED
+
+
 
 
